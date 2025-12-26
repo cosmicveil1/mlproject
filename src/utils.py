1986fdf18274673065e4b63d30db9273c97b59e7
Saumya269 <gsaumya2018@gmail.com>
2025-12-26 19:38:41 +0530

    hyperparamater training

--- src/utils.py
+++ src/utils.py
@@ -1,52 +1,65 @@
 import os
 import sys
 
-from sklearn.metrics import r2_score
-import dill
-from sklearn.metrics import r2_score
-import numpy as np
+import numpy as np 
 import pandas as pd
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-def save_object(file_path,obj):
-    '''
-    This function is responsible for saving the object to the file path
-    '''
+def save_object(file_path, obj):
     try:
-        dir_path=os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
+        dir_path = os.path.dirname(file_path)
 
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
+        os.makedirs(dir_path, exist_ok=True)
 
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
-
-
-def evaluate_models(X_train,y_train,X_test,y_test,models):
-    '''
-    This function is responsible for evaluating the models
-    '''
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
-        report={}
+        report = {}
 
-        for i in range(len(models)):
-            model=list(models.values())[i]
-            model.fit(X_train,y_train)
+        keys = list(models.keys())
+        values = list(models.values())
 
-            y_train_pred=model.predict(X_train)
+        for i in range(len(keys)):
+            key = keys[i]
+            model = values[i]
+            para = param.get(key, None)
 
-            y_test_pred=model.predict(X_test)
+            if para and len(para) > 0:
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
+            else:
+                # No hyperparameter grid; fit directly
+                model.fit(X_train, y_train)
 
-            train_model_score=r2_score(y_train,y_train_pred)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
 
-            test_model_score=r2_score(y_test,y_test_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]]=test_model_score
+            report[key] = test_model_score
 
         return report
 
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
